567721bc8d87b09dff05e4e86728f0df17fcc86c
Colas Gosselin <colas.gosselin@epita.fr>
2021-06-02 15:51:27 +0200

    add sentry

--- manager.js
+++ manager.js
@@ -1,6 +1,38 @@
 const MM = require('ms-manager');
 let config = require(`./config.json`) || {};
 
+const Sentry = require("@sentry/node");
+// or use es6 import statements
+// import * as Sentry from '@sentry/node';
+
+const Tracing = require("@sentry/tracing");
+// or use es6 import statements
+// import * as Tracing from '@sentry/tracing';
+
+Sentry.init({
+    dsn: "https://de89da8c42fd4c849e399bcc41936db2@o776203.ingest.sentry.io/5797322",
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+    op: "test",
+    name: "My First Test Transaction",
+});
+
+setTimeout(() => {
+    try {
+        foo();
+    } catch (e) {
+        Sentry.captureException(e);
+    } finally {
+        transaction.finish();
+    }
+}, 99);
+
 MM.init(config, (err, serviceInfo) => {
     if (err) {
         return console.error(err);
